a8e8586c946a4cb7d47ab05cac7a1ec6941327db
jubayed131 <jubayed.cste@gmail.com>
2018-11-25 18:44:23 +0100

    Selectione Sort Added

---
+++ DataStructAlgo/sorting/selection_sort/SelectionSort.java
@@ -0,0 +1,35 @@
+public class SelectionSort{
+  public static void main(String args[]){
+      int[] array={22,45,-34,45,-10,79,89};
+
+      for(int lastUnsortedIndex=array.length-1; lastUnsortedIndex>0;lastUnsortedIndex--){
+          int largest=0;
+        for(int i=0;i<=lastUnsortedIndex;i++){ // if we don't put equal sign
+
+          if(array[i]>array[largest]){
+            largest=i;
+
+          }
+        }
+
+        swap(array,lastUnsortedIndex, largest);
+      }
+
+
+      for(int i=0; i<array.length;i++){
+
+            System.out.println(array[i]);
+      }
+
+  }
+
+
+  public static void swap(int[] array, int i,int j){
+      int temp= array[i];
+      array[i]=array[j];
+      array[j]=temp;
+
+  }
+
+
+}
